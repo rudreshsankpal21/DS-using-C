44855492e160d97ac5d0e4245c37b5619505f5bf
rudresh21 <rudreshsankpal21@gmail.com>
2025-09-24 22:36:00 +0530

    practice linear queue

--- QUEUE/LinearQueue.c
+++ QUEUE/LinearQueue.c
@@ -1,94 +1,87 @@
-// Linear Queue using array
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
-#define MAXsize 5
+#define MAXSIZE 5
+
 void insert();
 void delete();
 void display();
-int front = -1 , rear = -1;
-int queue[MAXsize];
+
+int queue[MAXSIZE];
+int rear = -1 , front = -1;
+
 
 void main(){
     int choice;
-    while (choice !=  4){
+    while (choice != 4)
+    {
         printf("\n1.Insert\n2.Delete\n3.Display\n4.Exit\n");
         printf("Enter your choice\n");
         scanf("%d",&choice);
         switch (choice)
         {
-        case 1:
-            insert();
-            break;
-        case 2:
-            delete();
-            break;
-        case 3:
-            display();
-            break;
-        case 4:
-            exit(0);
-            break;
-        default:
-            printf("Invalid choice\n");
-            break;
+            case 1 : insert();
+                    break;
+            case 2 : delete();
+                    break;
+            case 3 : display();
+                    break;
+            case 4 : exit(0);
+                    break;
+            default : printf("Invalid choice\n");
+                    break;
         }
     }
-    
 }
-
 
 void insert(){
     int item;
-    printf("Enter the element to be inserted\n");
-    scanf("%d",&item);
-    if (rear == MAXsize - 1)
-    {
+    printf("Enter the element\n");
+    scanf("\n%d",&item);
+    if(rear == MAXSIZE - 1){
         printf("Queue is full\n");
         return;
-    }
-    if (front == -1 && rear == -1)
-    {
-        front = 0;
-        rear = 0;
-    }else {
-        rear ++;
+    } else if (front == -1 && rear == -1)
+        {
+            front = 0;
+            rear = 0;
+            queue[rear] = item;
+    } else {
+        rear += 1;
     }
     queue[rear] = item;
-    printf("Value inserted\n");   
+    printf("Element inserted successfully\n");
 }
 
 void delete(){
     int item;
-    if (front == -1 || front > rear)
+    if (front == - 1 && rear == -1)
     {
-        printf("\nQueue is empty");
+        printf("Queue is empty\n");
     } else {
-        item =  queue[front];
+        item = queue[front];
+        printf("Element deleted successfully\n");
         if (front == rear)
         {
             front = -1;
             rear = -1;
         } else {
-            front ++;
+            front += 1;
         }
-        printf("\nValue deleted successfully\n");
-        
+        printf("Element deleted successfully\n");
     }
     
 }
 
-void  display(){
+void display(){
     int i;
-    if (rear == -1)
+    if (front == -1 && rear == -1)
     {
-        printf("\nQueue is empty");
-    } else {
-        printf("\nElements of queue are\n");
+        printf("Queue is empty\n");
+    } 
+        printf("Elements in queue are\n");
         for ( i = front; i <= rear; i++)
         {
-            printf("\n%d",queue[i]);
-        }
-    }
-    
+            printf("%d\n",queue[i]);
+        }        
 }
