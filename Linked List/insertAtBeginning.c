93fb43497b8247104d27e09988d2c9bbcf147f0e
rudresh21 <rudreshsankpal21@gmail.com>
2025-09-30 22:28:25 +0530

    practiced insertAtBeginning in LL

--- Linked List/insertAtBeginning.c
+++ Linked List/insertAtBeginning.c
@@ -2,37 +2,38 @@
 #include <conio.h>
 #include <stdlib.h>
 
-struct node{
+struct node {
     int data;
     struct node *next;
 };
 
 struct node *head,*tail,*temp,*newNode;
 
-void insertAtBeginning(struct node**head, int val){
+void insertAtBeginning( struct node**head ,int val){
     newNode = (struct node*)malloc(sizeof(struct node));
-
     newNode -> data = val;
     newNode -> next = *head;
 
-    *head = newNode;
+    *head =  newNode;
 }
 
-void printList(){
+void display(){
     temp = head;
     while (temp != NULL)
     {
-        printf("%d -> ",temp -> data);
+        printf("%d -> ", temp -> data);
         temp = temp -> next;
     }
-    
+    printf("NULL");
 }
 
+
 int main(){
-    insertAtBeginning(&head,50);
-    insertAtBeginning(&head,40);
     insertAtBeginning(&head,30);
-    insertAtBeginning(&head,20);
     insertAtBeginning(&head,10);
-    printList();
+    insertAtBeginning(&head,20);
+    insertAtBeginning(&head,120);
+    insertAtBeginning(&head,60);
+    display();
+    return 0;
 }
