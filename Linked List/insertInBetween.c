f81d7246a04d67e0182148b0aeb7cfbb0073fcea
rudresh21 <rudreshsankpal21@gmail.com>
2025-10-02 20:25:02 +0530

    practiced insertionInBetween in LL

--- Linked List/insertInBetween.c
+++ Linked List/insertInBetween.c
@@ -3,40 +3,47 @@
 #include <stdlib.h>
 
 struct node {
-int data;
-struct node *next;
+    int data;
+    struct node *next;
 };
 
 struct node *head,*tail,*temp,*newNode;
 
-void insetInBetween(struct node *prevNode, int val){
+void insertInBetween(struct node *prevNode,int val){
     newNode = (struct node*)malloc(sizeof(struct node));
+
     if (prevNode == NULL)
     {
-        printf("Previous node cannot be NULL\n");
+        printf("Previous node does not exist\n");
+        return;
     }
     
+
     newNode -> data = val;
     newNode -> next = prevNode -> next;
+
     prevNode -> next = newNode;
 }
 
+
 void display(){
     temp = head;
+
     while (temp != NULL)
     {
-        printf("%d -> " , temp -> data);
+        printf("%d -> ", temp -> data);
         temp = temp -> next;
     }
     printf("NULL\n");
 }
 
 
-int main(){
+int main(int argc, char const *argv[])
+{
     head = (struct node*)malloc(sizeof(struct node));
     struct node *second = (struct node*)malloc(sizeof(struct node));
     struct node *third = (struct node*)malloc(sizeof(struct node));
-    
+    struct node *fourth = (struct node*)malloc(sizeof(struct node));
 
     head -> data = 10;
     head -> next = second;
@@ -44,13 +51,17 @@ int main(){
     second -> data = 20;
     second -> next = third;
 
-    third  -> data = 30;
-    third -> next = NULL;
+    third -> data = 30;
+    third -> next = fourth;
+
+    fourth -> data = 40;
+    fourth -> next = NULL;
+
 
     printf("Original Linked List\n");
     display();
-    insetInBetween(head, 15);
-    printf("Linked List after insertion\n");
+    insertInBetween(head,25);
+    printf("\nLinked List after insertion in between\n");
     display();
     return 0;
 }
