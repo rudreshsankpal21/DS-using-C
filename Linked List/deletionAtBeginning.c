5a4f8471094c5aa44523575f43050cdde43e499b
rudresh21 <rudreshsankpal21@gmail.com>
2025-10-02 20:49:59 +0530

    practiced deletionAtBeginning in LL

--- Linked List/deletionAtBeginning.c
+++ Linked List/deletionAtBeginning.c
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 
 struct node {
-int data;
-struct node *next;
+    int data;
+    struct node *next;
 };
 
 struct node *head,*tail,*temp,*newNode;
@@ -14,40 +14,48 @@ void deleteAtBeginning(struct node **head){
 
     if (*head == NULL)
     {
-        printf("List is empty\n");
-    }
-    
+        printf("List is empty ");
+    } 
+
     *head = (*head) -> next;
     free(temp);
 }
 
 
-void printLL(struct node *head){
-    while ( head != NULL){
-        printf("%d -> ",head -> data);
-        head = head -> next;
+void display(struct node *head){
+    temp = head;
+
+    while (temp != NULL)
+    {
+        printf("%d -> ", temp -> data);
+        temp = temp -> next;
     }
     printf("NULL\n");
 }
 
-
-int main(int argc, char const *argv[])
+int main()
 {
     head = (struct node*)malloc(sizeof(struct node));
     struct node *second = (struct node*)malloc(sizeof(struct node));
     struct node *third = (struct node*)malloc(sizeof(struct node));
+    struct node *fourth = (struct node*)malloc(sizeof(struct node));
 
     head -> data = 10;
     head -> next = second;
+
     second -> data = 20;
     second -> next = third;
-    third -> data = 30;
-    third -> next = NULL;
 
-    printf("Before Deleting : ");
-    printLL(head);
+    third -> data = 30;
+    third -> next = fourth;
+
+    fourth -> data = 40;
+    fourth -> next = NULL;
+
+    printf("Original Linked List\n");
+    display(head);
     deleteAtBeginning(&head);
-    printf("After Deleting : ");
-    printLL(head);
+    printf("\nLinked List after deletion at beginning\n");
+    display(head);
     return 0;
 }
