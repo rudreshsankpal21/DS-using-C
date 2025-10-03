5d476a7875815351f08f307fa36ba7fad35fd10f
rudresh21 <rudreshsankpal21@gmail.com>
2025-10-03 16:34:35 +0530

    practiced deletion at end in LL

--- Linked List/deleteAtEnd.c
+++ Linked List/deleteAtEnd.c
@@ -3,58 +3,66 @@
 #include <stdlib.h>
 
 struct node {
-int data;
-struct node* next;
+    int data;
+    struct node* next; 
 };
+
+struct node* head,*temp,*tail,*newNode;
 
 
 struct node* createNode(int val){
-    struct node* newNode = (struct node*)malloc(sizeof(struct node));
+
+    newNode = (struct node*)malloc(sizeof(struct node));
     newNode -> data = val;
     newNode -> next = NULL;
+
     return newNode;
 }
 
+
 struct node* deleteAtEnd(struct node* head){
-if (head == NULL)
-{
-    printf("List is empty\n");
-    return NULL;
-} 
-if(head-> next == NULL){
-    free(head);
-    return NULL;
+    if (head == NULL)
+    {
+        return NULL;
+    }
+    
+    if (head -> next == NULL)    {
+        free(head);
+        return NULL;
+    }
+
+    struct node* second_last = head;
+
+    while (second_last -> next -> next != NULL)
+    {
+        second_last = second_last -> next;
+    }
+
+    free(second_last -> next);
+    second_last -> next = NULL;
+
+    return head;
 }
 
-struct node* second_last = head;
-while (second_last -> next -> next != NULL)
-{
-    second_last = second_last -> next;
-}
-
-free(second_last -> next);
-second_last -> next = NULL;
-
-        
-}
-
-void printList(struct node *head){
+struct node* printList(struct node* head){
     while (head != NULL)
     {
-        printf("%d -> ",head -> data);
+        printf("%d -> ", head -> data);
         head = head -> next;
     }
     printf("NULL\n");
 }
 
 int main(){
-    struct node* head = createNode(1);
-    head -> next = createNode(2);
-    head -> next -> next = createNode(3);
+   struct node* head = createNode(10);
+    head -> next = createNode(20);
+    head -> next -> next = createNode(30);
+    // head -> next -> next -> next = createNode(40);
+    // head -> next -> next -> next -> next = createNode(50);
     printf("Original Linked List\n");
     printList(head);
-    deleteAtEnd(head);
-    printf("Linked List after deletion at end\n");
+    head = deleteAtEnd(head);
+    printf("\nLinked List after deletion at end\n");
     printList(head);
     return 0;
 }
