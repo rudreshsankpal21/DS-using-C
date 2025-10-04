58cc807c68b9f6399490dbf0686cb6e4720b6800
rudresh21 <rudreshsankpal21@gmail.com>
2025-10-04 22:21:01 +0530

    practiced insertAtBeginning in LL

--- Doubly LL/insertAtBeginning.c
+++ Doubly LL/insertAtBeginning.c
@@ -2,44 +2,42 @@
 #include <conio.h>
 #include <stdlib.h>
 
-struct node {
+struct node{
 int data;
-struct node* prev;
 struct node* next;
+ struct node* prev; 
 };
 
-struct node* insertAtBeginning(struct node* head,int val){
+struct node* insertAtBeginning(struct node* head, int val){
     struct node* newNode = (struct node*)malloc(sizeof(struct node));
 
     newNode -> data = val;
+    newNode -> next = head;
     newNode -> prev = NULL;
-    newNode->next = head;
+
     if (head != NULL)
     {
-        head ->prev = newNode;
+        head -> prev = newNode;
     }
 
     head = newNode;
     return head;
-}
-
+} 
 
 void display(struct node* head){
-    while (head != NULL)
+    struct node* temp = head;
+    while (temp != NULL)
     {
-        printf("%d -> ", head->data);
-        head = head->next;
+        printf("%d -> ",temp -> data);
+        temp = temp -> next;
     }
     printf("NULL\n");
-    
 }
 
-int main(int argc, char const *argv[])
-{
+int main(){
     struct node* head = NULL;
-    head = insertAtBeginning(head,20);
     head = insertAtBeginning(head,10);
-    head = insertAtBeginning(head,40);
+    head = insertAtBeginning(head,20);
     head = insertAtBeginning(head,30);
     display(head);
     return 0;
